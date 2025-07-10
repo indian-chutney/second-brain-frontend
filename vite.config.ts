c9e8af053777475772c05422979e5b4838cd497f
indian-chutney <zubairshaik2615@gmail.com>
2025-07-10 22:32:31 +0530

    change in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  define: {
-    "process.env.VITE_EMBED_KEY": JSON.stringify(process.env.VITE_EMBED_KEY),
-    "process.env.VITE_BACKEND_URL": JSON.stringify(
-      process.env.VITE_BACKEND_URL,
-    ),
-  },
 });
